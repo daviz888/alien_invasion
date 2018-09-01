2018fad50516b729a4cd5b1b86dfbf38dbf7941d
Daffy McBlood <ayos888@gmail.com>
2018-09-01 15:29:46 +0300

    add files

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings():
+    """ A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initializae the game's settings."""
+        # Screen settings
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (135, 206, 250)
+
+        #Ship Settings
+        self.ship_speed_factor = .5
+
+        
